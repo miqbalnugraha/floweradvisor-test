526b52be34891a479efc50d2648ab27c8d62f900
MyBook11G <iqbalturmudzi@gmail.com>
2022-04-10 11:23:09 +0700

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-12">
+            <div class="col-md-12" style="max-width: 1024px;">
                 <div class="card">
                     <div class="card-header text-center">
                        <h4>Home</h4>
